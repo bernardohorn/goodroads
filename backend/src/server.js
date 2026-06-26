3ebf4587b3a38a4736ad64187717e632adb5a3d5
Certoeerado <134650057+Certoeerado@users.noreply.github.com>
2026-06-26 09:02:53 -0300

    Pequenos ajustes

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ require('dotenv').config();
 const pool = require('./config/banco');
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // ─── Middleware ───────────────────────────────────────────────────────────────
 
